eff15892b6fd0e4005a50a1bc95ba8ca0717cdd4
Ertussy <riccardo.ferro-2@studenti.unitn.it>
2026-04-23 10:45:16 +0200

    Uploaded example for RFID

--- include/project_config.h
+++ include/project_config.h
@@ -9,5 +9,12 @@
 
 #define BUTTON 0 // GPIO0 is often used for a button on ESP32 boards
 
+#define LED1 27
+#define LED2 4
+
+#define KEY1 34
+#define SW1 35
+#define RP1 33
+
 
 #endif // __PROJECT_CONFIG_H__
